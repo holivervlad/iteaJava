e579a56b518afe26d2f576fed447d06129f63334
Vlad Holiver <vladyslav.holiver@ctdev.io>
2020-11-15 15:39:43 +0200

    third commit in master

--- src/main/java/main/MainFrame.java
+++ src/main/java/main/MainFrame.java
@@ -8,6 +8,7 @@ public class MainFrame extends JFrame {
         new MyFrame();
         System.out.println("first commit in master");
         System.out.println("second commit in master");
+        System.out.println("third commit in master");
 
     }
 }
