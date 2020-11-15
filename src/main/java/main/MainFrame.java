4f1f5d0a5ec2df640a9879834c12dd5bcdfa9bee
Vlad Holiver <vladyslav.holiver@ctdev.io>
2020-11-15 19:14:06 +0200

    jar file is not opened error

--- src/main/java/main/MainFrame.java
+++ src/main/java/main/MainFrame.java
@@ -6,10 +6,5 @@ public class MainFrame extends JFrame {
 
     public static void main(String[] args) {
         new MyFrame();
-        System.out.println("first commit in master");
-        System.out.println("second commit in master");
-        System.out.println("third commit in master");
-        System.out.println("fourth commit in master");
-
     }
 }
--- src/main/java/main/MyFrame.java
+++ src/main/java/main/MyFrame.java
@@ -7,151 +7,40 @@ import java.awt.event.ActionListener;
 import java.io.*;
 
 public class MyFrame extends JFrame {
-    private static MyFrameOverlay myFrameOverlay = new MyFrameOverlay();
-    private static final String GERMAN_FLAG_IMAGE_NAME = "FLAG_OF_GERMAN.png";
-    private static final String AMERICAN_FLAG_IMAGE_NAME = "FLAG_OF_USA.png";
-    private static final String UKRAINE_FLAG_IMAGE_NAME = "FLAG_OF_UKRAINE.png";
-
-    private String firstLabelString = "Це";
-    private String secondLabelString = " э ";
-    private String thirdLabelString = "украiнська";
-
-    private JLabel firstLabel = new JLabel(firstLabelString);
-    private JLabel secondLabel = new JLabel(secondLabelString);
-    private JLabel thirdLabel = new JLabel(thirdLabelString);
-
-    private JButton saveButton, loadButton, button;
-    private JComponent ukraineFlag, germanFlag, americanFlag;
-
-    private String commonImagePath = "/Users/vladyslavholiver/Documents/itea/src/main/resources";
+    private String commonImagePath = "/Users/vladyslavholiver/Documents/itea/leasson2/src/main/resources";
     private String filePath = commonImagePath + "/data.txt";
 
     MyFrame() {
         super("ITEA");
-        setLayout(new GridLayout(3, 3));
-        addDescriptionToLayout();
-        changeDescriptionToLayout();
-        addButtonsToLayout();
-        setSize(1000, 1000);
+        setLayout(new GridLayout(1, 1));
+        getValueFromFile();
+        setSize(600, 600);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    private void addButtonsToLayout() {
-        saveButton = createSaveButton();
-        loadButton = createLoadButton();
-        add(saveButton);
-        add(loadButton);
+    private void getValueFromFile() {
+        JLabel getValueFromFile = new JLabel(getTextFromFileViaJarFile());
+        add(getValueFromFile);
     }
 
-    private void addDescriptionToLayout() {
-        ukraineFlag = addUkraineFlag();
-        germanFlag = addGermanFlag();
-        americanFlag = addAmericanFlag();
-
-        add(ukraineFlag);
-        add(germanFlag);
-        add(americanFlag);
-    }
-
-    private void changeDescriptionToLayout() {
-        add(firstLabel);
-        add(secondLabel);
-        add(thirdLabel);
-    }
-
-    public void setTextToComponents() {
-        firstLabel.setText(firstLabelString);
-        secondLabel.setText(secondLabelString);
-        thirdLabel.setText(thirdLabelString);
-    }
-
-    private JButton addGermanFlag() {
-        button = myFrameOverlay.addImageToLayOut(GERMAN_FLAG_IMAGE_NAME);
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                firstLabelString = "Das";
-                secondLabelString = " ist ";
-                thirdLabelString = "deutsch";
-                setTextToComponents();
-            }
-        });
-        return button;
-    }
-
-    private JButton addAmericanFlag() {
-        button = myFrameOverlay.addImageToLayOut(AMERICAN_FLAG_IMAGE_NAME);
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                firstLabelString = "This";
-                secondLabelString = " is ";
-                thirdLabelString = "american";
-                setTextToComponents();
-            }
-        });
-        return button;
-    }
-
-    private JButton addUkraineFlag() {
-        button = myFrameOverlay.addImageToLayOut(UKRAINE_FLAG_IMAGE_NAME);
-            button.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    firstLabelString = "Це";
-                    secondLabelString = " э ";
-                    thirdLabelString = "украiнська";
-                    setTextToComponents();
-                }
-            });
-        return button;
-    }
-
-    public JButton createSaveButton() {
-        JButton button = new JButton("Save");
-        button.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(filePath));
-                    outputStream.writeBytes(firstLabelString);
-                    outputStream.writeBytes(secondLabelString);
-                    outputStream.writeBytes(thirdLabelString);
-
-                    System.out.println(String.format("File is written successfully with values: '%s', '%s', '%s'", firstLabelString, secondLabelString, thirdLabelString));
-                } catch (IOException j) {
-                    j.printStackTrace();
-                }
-            }
-        });
-        return button;
-    }
-
-    public JButton createLoadButton() {
-        JButton button = new JButton("Load");
-        button.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-                try {
+    public String getTextFromFileViaJarFile() {
+        String[] values = new String[3];
+        try {
                     File file = new File(filePath);
                     FileInputStream fileInputStream = new FileInputStream(file);
                     byte[] data = new byte[fileInputStream.available()];
                     fileInputStream.read(data);
                     fileInputStream.close();
                     String str = new String(data);
-                    String[] values = str.split(" ");
+                    values = str.split(" ");
                     if (values.length > 0) {
                         System.out.println("File is read successfully");
                     }
-                    firstLabel.setText(values[0]);
-                    secondLabel.setText(values[1]);
-                    thirdLabel.setText(values[2]);
-
                     System.out.println("Previous saved values are set successfully");
-
                 } catch (IOException j) {
                     System.out.println("File is empty");
                 }
-            }
-        });
-        return button;
+        return values[0] + " " + values[1] + " " + values[2];
     }
 }
